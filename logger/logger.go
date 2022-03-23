fc005f1371648506061f74872fec2aca7d1e2d5b
Naren <narencodes@pm.me>
2022-03-23 17:47:44 +0530

    Provide wrapped global logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,5 +1,7 @@
 package logger
 
+import "sync"
+
 // Logger exposes all log functions.
 type Logger interface {
 	Info(msg string, fields Field)
@@ -9,9 +11,16 @@ type Logger interface {
 	Fatal(msg string, fields Field)
 }
 
+//nolint
+var (
+	zlog    Logger
+	zapOnce sync.Once
+)
+
 // The semi structured log fields.
 type Field map[string]interface{}
 
+// NewZapLogger returns a zap logger instance.
 //nolint:ireturn // Return interface to protect zap methods.
 func NewZapLogger(service string) Logger {
 	z, err := newZap(service)
@@ -20,4 +29,27 @@ func NewZapLogger(service string) Logger {
 	}
 
 	return z
+}
+
+// NewZapGlobal returns a global zap logger instance.
+// When called multiple times it will return same object with the
+// service name of first caller.
+// **This primarily to use the logger in code where we can't pass around
+// logger from main function. Avoid globals:)**.
+func NewZapGlobal(service string) Logger {
+	if service == "" {
+		panic(errEmptyService)
+	}
+
+	getZap := func() {
+		var err error
+		zlog, err = newZap(service)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+	zapOnce.Do(getZap)
+
+	return zlog
 }
